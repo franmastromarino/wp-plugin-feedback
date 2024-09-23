5470f67c88546cae1ed43b5afe1808c47659c6d6
Francisco <6652831+franmastromarino@users.noreply.github.com>
2024-09-23 21:01:20 +0200

    fix: build

--- build/js/index.asset.php
+++ build/js/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-components', 'wp-element', 'wp-i18n'), 'version' => '1607bd7e3a7a2aac638a');
+<?php return array('dependencies' => array('wp-components', 'wp-element', 'wp-i18n'), 'version' => 'f6682a1fc0126bec8fb7');
