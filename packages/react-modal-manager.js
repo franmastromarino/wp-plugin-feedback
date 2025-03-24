631d2f794cef3447b0f51a9916974b74ba4303c6
Francisco <6652831+franmastromarino@users.noreply.github.com>
2025-03-24 08:59:55 +0100

    fix: wp 5.9.x compatibility

--- packages/react-modal-manager.js
+++ packages/react-modal-manager.js
@@ -1,83 +1,92 @@
-import { createRoot, useState } from '@wordpress/element';
-import DeactivationModal from './components/DeactivationModal';
+import { createRoot, render, useState } from "@wordpress/element";
+import DeactivationModal from "./components/DeactivationModal";
 
 const ModalManager = () => {
-	const [isModalOpen, setIsModalOpen] = useState(false);
-	const [plugin, setPlugin] = useState(null);
-	const [isSubmitting, setIsSubmitting] = useState(false); // Handle form submission state
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  const [plugin, setPlugin] = useState(null);
+  const [isSubmitting, setIsSubmitting] = useState(false); // Handle form submission state
 
-	// Expose a global method to open the modal
-	window.ReactModalManager = {
-		open: (pluginData) => {
-			setPlugin(pluginData);
-			setIsModalOpen(true);
-		},
-		close: () => {
-			setIsModalOpen(false);
-			setPlugin(null);
-		},
-	};
+  // Expose a global method to open the modal
+  window.ReactModalManager = {
+    open: (pluginData) => {
+      setPlugin(pluginData);
+      setIsModalOpen(true);
+    },
+    close: () => {
+      setIsModalOpen(false);
+      setPlugin(null);
+    },
+  };
 
-	const handleSubmit = async ({ reason, details, isAnonymous, hasFeedback }) => {
-		setIsSubmitting(true);
-		// Preparar la solicitud fetch
-		fetch(window.ajaxurl, {
-			method: 'POST',
-			headers: {
-				'Content-Type': 'application/x-www-form-urlencoded',
-			},
-			body: new URLSearchParams({
-				action: 'quadlayers_send_feedback',
-				plugin_basename: plugin.pluginBasename,
-				feedback_reason: reason,
-				feedback_details: details,
-				is_anonymous: isAnonymous,
-				has_feedback: hasFeedback,
-				nonce: window.quadlayersPluginFeedback.nonce,
-			}),
-		})
-			.then((response) => response.json())
-			.then((data) => {
-				if (process.env.NODE_ENV === 'development') {
-					if (data.success) {
-						console.log('Success:', data.data.message);
-					} else {
-						console.error('Error:', data.data.message);
-					}
-				}
-			})
-			.catch((error) => {
-				if (process.env.NODE_ENV === 'development') {
-					console.error('Error en la solicitud fetch:', error);
-				}
-			})
-			.finally(() => {
-				// Redirect to the deactivation URL
-				window.location.href = plugin.deactivateUrl;
-				setIsSubmitting(false);
-			});
-	};
+  const handleSubmit = async ({
+    reason,
+    details,
+    isAnonymous,
+    hasFeedback,
+  }) => {
+    setIsSubmitting(true);
+    // Preparar la solicitud fetch
+    fetch(window.ajaxurl, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/x-www-form-urlencoded",
+      },
+      body: new URLSearchParams({
+        action: "quadlayers_send_feedback",
+        plugin_basename: plugin.pluginBasename,
+        feedback_reason: reason,
+        feedback_details: details,
+        is_anonymous: isAnonymous,
+        has_feedback: hasFeedback,
+        nonce: window.quadlayersPluginFeedback.nonce,
+      }),
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        if (process.env.NODE_ENV === "development") {
+          if (data.success) {
+            console.log("Success:", data.data.message);
+          } else {
+            console.error("Error:", data.data.message);
+          }
+        }
+      })
+      .catch((error) => {
+        if (process.env.NODE_ENV === "development") {
+          console.error("Error en la solicitud fetch:", error);
+        }
+      })
+      .finally(() => {
+        // Redirect to the deactivation URL
+        window.location.href = plugin.deactivateUrl;
+        setIsSubmitting(false);
+      });
+  };
 
-	return (
-		<>
-			{isModalOpen && (
-				<DeactivationModal
-					onClose={() => setIsModalOpen(false)}
-					onSubmit={handleSubmit}
-					isSubmitting={isSubmitting}
-				/>
-			)}
-		</>
-	);
+  return (
+    <>
+      {isModalOpen && (
+        <DeactivationModal
+          onClose={() => setIsModalOpen(false)}
+          onSubmit={handleSubmit}
+          isSubmitting={isSubmitting}
+        />
+      )}
+    </>
+  );
 };
 
 // Render the modal manager into the DOM
-document.addEventListener('DOMContentLoaded', () => {
-	const body = document.getElementById('wpbody-content');
+document.addEventListener("DOMContentLoaded", () => {
+  const body = document.getElementById("wpbody-content");
 
-	const container = document.createElement('div');
+  const container = document.createElement("div");
 
-	body.insertBefore(container, body.lastElementChild);
+  body.insertBefore(container, body.lastElementChild);
 
-	createRoot(container).render(<ModalManager />);
+  if (createRoot !== undefined) {
+    createRoot(container).render(<ModalManager />);
+  } else {
+    render(<ModalManager />, container);
+  }
 });
